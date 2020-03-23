e5d1ad783b3d74c53a505af92d88d09f24473b8a
woowonjin <45147364+woowonjin@users.noreply.github.com>
2020-03-24 02:08:45 +0900

    I'm doing about reservation

--- reservations/models.py
+++ reservations/models.py
@@ -2,6 +2,19 @@ from django.db import models
 from django.utils import timezone
 from core import models as core_models
 import datetime
+from . import managers
+
+
+class BookedDay(core_models.TimeStampedModel):
+    day = models.DateField()
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Booked Day"
+        verbose_name_plural = "Booked Days"
+
+    def __str__(self):
+        return f"{self.reservation.room} / {self.day}"
 
 
 class Reservation(core_models.TimeStampedModel):
@@ -16,6 +29,7 @@ class Reservation(core_models.TimeStampedModel):
         (STATUS_CONFIRM, "Confirmed"),
         (STATUS_CANCELED, "Canceled"),
     )
+    objects = managers.CustomReservationManager()
 
     status = models.CharField(
         max_length=12, choices=STATUS_CHOICES, default=STATUS_PENDING
@@ -46,3 +60,23 @@ class Reservation(core_models.TimeStampedModel):
         return now > self.check_out
 
     is_finished.boolean = True
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            start = self.check_in
+            end = self.check_out
+            difference = end - start
+            print("!!!!!!!!!")
+            print("!!!!!!!!!")
+            existing_booked_day = BookedDay.objects.filter(
+                reservation__room=self.room, day__range=(start, end)
+            ).exists()
+            print("????????????")
+            if not existing_booked_day:
+                super().save(*args, **kwargs)
+                for i in range(difference.days + 1):
+                    day = start + datetime.timedelta(days=i)
+                    BookedDay.objects.create(day=day, reservation=self)
+                print(">>>>>>>>>>>>>>>????!!!")
+                return
+            return super().save(*args, **kwargs)
